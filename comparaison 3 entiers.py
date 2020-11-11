1dfda32dc5aec4daf34a1ccd7a1cd159ca1a8f67
aromdhane <ahlem.romdhane@le-campus-numerique.fr>
2020-11-11 17:07:44 +0100

    code

---
+++ comparaison 3 entiers.py
@@ -0,0 +1,12 @@
+
+
+print ("Maximum de trois entiers")
+a=int (input("1er entier="))
+b=int (input("2eme entier="))
+c=int (input("3eme entier="))
+if a>=b and a>=c :
+ print("le maximum est :",a)
+elif a<=b and b>=c :
+    print ("le maximum est :",b)
+else :
+    print("le maximum est:",c)
